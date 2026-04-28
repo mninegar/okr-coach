1235e3d605cab1a61e437c2b3473e9cc4626982f
mninegar <mmninegar@msn.com>
2026-04-28 16:43:19 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect, useCallback } from "react";
 import Head from "next/head";
 
-const FONT = "'NeueKabel', 'Barlow', sans-serif";
+const FONT = "'Nunito', 'NeueKabel', sans-serif";
 
 // ── Design tokens ─────────────────────────────────────────────────────────────
 const Tk = {
