50c48819e0c039357b9d1d05f49f1e94a590620e
mninegar <mmninegar@msn.com>
2026-04-28 14:57:44 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,10 +54,9 @@ export default function OKRChatbot(){
   const restart=useCallback(()=>{setMsgs([{role:"bot",isWelcome:true}]);setHistory([]);setInput("");setProgress(5);clearDoc();},[clearDoc]);
   const onKey=e=>{if(e.key==="Enter"&&!e.shiftKey){e.preventDefault();send();}};
   return(<><Head><title>OKR Coach - Once Upon a Farm</title></Head>
-    <div style={{minHeight:"100vh",background:Tk.cream,display:"flex",justifyContent:"center",alignItems:"flex-start",padding:"32px 16px",fontFamily:FONT}}>
-      <div onDragEnter={onDE} onDragLeave={onDL} onDragOver={onDO} onDrop={onDrop} style={{width:"100%",maxWidth:760,background:Tk.cream,display:"flex",flexDirection:"column",borderRadius:18,overflow:"hidden",border:drag?`1.5px solid ${Tk.sprout}`:`0.5px solid ${Tk.line}`,position:"relative",transition:"border-color 0.15s"}}>
+    <div style={{minHeight:"100vh",background:Tk.cream,display:"flex",justifyContent:"center",alignItems:"flex-start",padding:"0 0 32px 0",fontFamily:FONT}}>
+      <div onDragEnter={onDE} onDragLeave={onDL} onDragOver={onDO} onDrop={onDrop} style={{width:"100%",maxWidth:760,background:Tk.cream,display:"flex",flexDirection:"column",borderRadius:18,overflow:"hidden",marginTop:24,border:drag?`1.5px solid ${Tk.sprout}`:`0.5px solid ${Tk.line}`,position:"relative",transition:"border-color 0.15s"}}>
         {/* Ambient produce - exact positions from layout1. No zIndex = renders below zIndex:2 content */}
-        <img src={IMG.kale_leaf}    alt="" style={{position:"absolute",left:-34, bottom:90, width:130,opacity:.10,transform:"rotate(-18deg)",pointerEvents:"none",userSelect:"none"}}/>
         <img src={IMG.banana_slice} alt="" style={{position:"absolute",right:-28,top:130,  width:120,opacity:.10,transform:"rotate(18deg)", pointerEvents:"none",userSelect:"none"}}/>
         <img src={IMG.pumpkin}      alt="" style={{position:"absolute",right:-50,bottom:-30,width:180,opacity:.09,transform:"rotate(-8deg)", pointerEvents:"none",userSelect:"none"}}/>
         <img src={IMG.avocado}      alt="" style={{position:"absolute",right:-22,top:180,   width:90, opacity:.10,transform:"rotate(-14deg)", pointerEvents:"none",userSelect:"none"}}/>
