b04f6d7c8b0dd808db22a0357c7f09d8553bc415
mninegar <mmninegar@msn.com>
2026-04-28 14:46:14 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,19 @@
+import localFont from 'next/font/local';
 import '../styles/globals.css';
+
+const neueKabel = localFont({
+  src: [
+    { path: '../public/fonts/NeueKabel-Regular.otf', weight: '400', style: 'normal' },
+    { path: '../public/fonts/NeueKabel-Bold.otf', weight: '700', style: 'normal' },
+  ],
+  display: 'block',
+  variable: '--font-neue-kabel',
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={neueKabel.className} style={{ minHeight: '100vh', background: '#FBF7E1' }}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
